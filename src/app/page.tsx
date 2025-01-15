f3a0c0b72deafcb30e42b30adf3cd4bfe06cba2a
Rasmus Lauritsen <rasmus.lauritsen.dev@outlook.com>
2025-01-15 14:52:34 +0100

    feat: replace hardcoded jsx with fetched data from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import { GameCover } from '@/components/game-cover'
 
-export default function Home() {
+type Game = {
+  id: number
+  name: string
+  cover: {
+    id: number
+    url: string
+  }
+}
+
+export default async function Home() {
+  const response = await fetch('https://api.igdb.com/v4/games', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Client-ID': process.env.IGDB_CLIENT_ID || '',
+      'Authorization': `Bearer ${process.env.IGDB_AUTHORIZATION || ''}`,
+    },
+    body: 'fields name, cover.url;',
+  })
+
+  const data: Game[] = await response.json()
+
   return (
     <div className="mx-auto max-w-7xl p-4">
       <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
-        {Array.from({ length: 9 }).map((_, index) => (
-          <GameCover key={index} />
+        {data.map((game) => (
+          <GameCover key={game.id} game={game} />
         ))}
       </div>
     </div>
--- src/components/game-cover.tsx
+++ src/components/game-cover.tsx
@@ -3,16 +3,34 @@ import Link from 'next/link'
 
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 
-export function GameCover() {
+type Game = {
+  id: number
+  name: string
+  cover: {
+    id: number
+    url: string
+  }
+}
+
+export function GameCover({ game }: { game: Game }) {
+  const coverUrl = game.cover?.url.replace('t_thumb', 't_cover_big')
+
   return (
     <div className="transition duration-300 ease-out hover:scale-105">
       <Link href="#" className="block">
         <AspectRatio ratio={3 / 4}>
-          <Image
-            src="https://images.igdb.com/igdb/image/upload/t_720p/co670h.jpg"
-            fill={true}
-            alt="Game Cover"
-          />
+          {coverUrl ? (
+            <Image
+              src={`https:${coverUrl}`}
+              className="rounded border"
+              fill
+              alt={`${game.name} Cover`}
+            />
+          ) : (
+            <div className="grid h-full place-items-center rounded border p-2">
+              <span className="text-center">{game.name}</span>
+            </div>
+          )}
         </AspectRatio>
       </Link>
     </div>
